541eed488eb1367ebfd84be86f0dfe918c540a06
juliMAB <julian6aguirre@hotmail.com>
2022-06-27 21:19:53 -0300

    Added plane, change mask.

--- Assets/Script/JuliScripts/HacksScene.cs
+++ Assets/Script/JuliScripts/HacksScene.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class HacksScene : MonoBehaviour
 {
-    public TMP_Text example;
-    public TMP_Text[] a;
+    public Image example;
+    public Button[] a;
     public void OnCall()
     {
-        for (int i = 0; i < a.Length; i++) 
-            a[i].font = example.font;
+        for (int i = 0; i < a.Length; i++)
+            a[i].targetGraphic.GetComponent<Image>().sprite = example.sprite;
     }
     public void OnFoundObjects()
     {
-        a = FindObjectsOfType<TMP_Text>();
+        a = FindObjectsOfType<Button>();
     }
 }
